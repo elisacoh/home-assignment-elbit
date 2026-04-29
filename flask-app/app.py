019f1626ca96a2f6f7bf428eadf7f655a2f75441
elisa.cohen <elisa.cohen05@gmail.com>
2026-04-29 19:52:22 +0300

    fix the seed namefile

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,7 +1,6 @@
 import docker
 from flask import Flask, jsonify
 
-client = docker.from_env()
 
 app = Flask(__name__)
 
@@ -11,10 +10,12 @@ talks to the local docker engine and gets the list of running containers
 @app.route("/containers")
 def get_running_containers():
     try:
+        client = docker.from_env()
         containers = client.containers.list()
+        return [{"name": c.name, "status": c.status, "id": c.short_id} for c in containers]
+    
     except docker.errors.DockerException as err:
         return {"error": str(err)}, 500
-    return [{"name": c.name, "status": c.status, "id": c.short_id} for c in containers]
 
 @app.route("/health")
 def health():
